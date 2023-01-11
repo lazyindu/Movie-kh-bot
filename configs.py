f5131e25b2d09837a38373c74266a9044ab72d86
indu <70087467+lazyindu@users.noreply.github.com>
2023-01-12 01:34:47 +0530

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,65 @@
+# (c) @RoyalKrrishna
+
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+
+class Config(object):
+    API_ID = int(os.getenv("API_ID", 27794931))
+    API_HASH = os.getenv("API_HASH", "64498a08cd474a139a8e5a1c5a5aa776")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5895490510:AAHJiL_6X0F_h1bps8qG8xvzFAUUIFT3jEg")
+    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "LazyDeveloperr")
+#   Telethon session string 
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOKkBu6TClreLLt-XXiBrrIeOAq6QrBnlyI7fnOldNSIYzEeiHgNEXSARRc9NoDvZ2mlLAGUMRNCgAs1Q2baoB0PA4FgmqREUy_CeslW0v-7qfpZiD4XhbJQwCvdIJljBzONZPETrmPe0NZNfL3tQwrn7PKvZzphT19iPjiD8kekGyTcVHk25pZ9axyYkd2tNC9TMo_-BWNsKxgAt1kbHQRGMKmsGAnFtYyyrcLOaSPjawL4dlz1opZMKy0xPnSdj1bcZAVbt2-YQ7Su8Qyl3IRj-J1-BdkHzrt41efGfxrgzNDEdHgiKAU4IFwG1rxEJxfy7njdqptw9ALPyX3KqtEFHiiQ=")
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001887632033))
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "Movie_kh_bot")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", 2031365529))
+#    OWNER_USERNAME = os.getenv("OWNER_USERNAME")
+    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "Bollywood_movies_3")
+#    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
+    START_MSG = os.getenv("START_MSG", '''Hᴇʏ Bᴜᴅᴅʏ! 😃
+
+I'ᴍ A Bᴏᴛ Fᴏʀ Sᴇɴᴅɪɴɢ Fʀᴏᴍ Yᴏᴜʀ Cʜᴀɴɴᴇʟ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.😚
+
+Yᴏᴜ Cᴀɴ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ.☺️
+
+Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
+    START_PHOTO = os.getenv("START_PHOTO", "https://telegra.ph/file/9fb99c91f6e887dbc4bd0.jpg")
+    HOME_TEXT = os.getenv("HOME_TEXT", '''ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕
+
+ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ʏᴏᴜʀ ʟɪɴᴋꜱ,
+ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅''')
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", None)
+    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://lazy:lazy@cluster0.mcsqp8q.mongodb.net/?retryWrites=true&w=majority")
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1001881747562))
+    RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
+    BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
+    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "Bollywood_movies_3")
+    FORCE_SUB = os.getenv("FORCE_SUB", "False")
+    AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 60))
+    MDISK_API = os.getenv("MDISK_API", "12334")
+    VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "365"))
+    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT", '''I ᴏɴʟʏ ꜱʜᴀʀᴇ ᴛʜᴇ ᴘᴏꜱᴛ ꜰʀᴏᴍ ᴘᴇᴏᴘʟᴇ'ꜱ ᴄʜᴀɴɴᴇʟ! ᴡʜᴏ ᴍᴀᴅᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, i ᴅᴏ ɴᴏᴛ ꜱᴛᴏʀᴇ ᴀɴʏ ꜰɪʟᴇꜱ ᴏʀ ᴛᴇxᴛ ɪɴ  ᴍʏ ᴅᴀᴛᴀʙᴀꜱᴇ.✅
+
+ᴅᴍ ꜰᴏʀ ᴀɴʏ Qᴜᴇʀʏ @Abdul_khalik_7''')
+    ABOUT_HELP_TEXT = os.getenv("HELP_TEXT", '''🍓 RᴇQᴜɪʀᴇᴍᴇɴᴛ ᴛᴏ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ!
+
+🍓 Sᴛᴇᴘ 1 - Aᴘᴋᴏ ᴇᴋ ɢʀᴏᴜᴘ ᴋɪ ᴊᴀʀᴜʀᴀᴛ ʜᴏɢɪ, ᴊɪꜱᴍᴇ ᴍᴇᴍʙᴇʀꜱ ʙʜɪ ʜᴏ, ᴀᴜʀ ᴇᴋ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴋɪ ᴊᴀʀᴜʀᴀᴛ ʜᴏɢɪ, ᴊɪꜱᴍᴇ ᴀᴘᴋᴇ ꜱᴀʀᴇ ᴘᴏꜱᴛ ʜᴏɴɢᴇ!
+
+🍓 Sᴛᴇᴘ 2 - ʙᴏᴛ ᴋᴏ ᴀᴘɴᴇ ɢʀᴏᴜᴘ ᴀᴜʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴋᴀ ᴀᴅᴍɪɴ ʙᴀɴᴀɴᴀ ʜᴏɢᴀ.
+
+🍓 Sᴛᴇᴘ 3 - ɢʀᴏᴜᴘ ᴍᴇ "/request" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ!
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @Abdul_khalik_7
+
+🍓 Sᴛᴇᴘ 4 - ɢʀᴏᴜᴘ ᴍᴇ "/addb - ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ɪᴅ" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ.
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @Abdul_khalik_7
+
+🍓 Sᴛᴇᴘ 5 - ᴀʙ ᴀᴘᴋᴏ ᴀᴘɴᴇ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴘᴏꜱᴛ ᴅᴀʟɴɪ ʜᴏɢɪ,
+ᴊɪꜱꜱᴇ ᴋɪ ᴀɢᴀʀ ɢʀᴏᴜᴘ ᴍᴇ ᴋᴏɪ ᴜꜱᴇʀ ꜱᴇᴀʀᴄʜ ᴋᴀʀᴇ ᴛᴏ ʏᴇ ʙᴏᴛ ᴜɴ ᴜꜱᴇʀ ᴋᴇ Qᴜᴀʀʏ ᴋᴏ ꜱᴀᴍᴀᴊʜ ᴋᴇ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ꜱᴇ ᴘᴏꜱᴛ ᴜᴛʜᴀ ᴋᴇ ᴜɴʜᴇ ᴅᴇ ᴘᴀʏᴇ.
+
+
+🍓 Nᴏᴛᴇ : Bᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴊᴏɪɴ ʜᴏɴᴇ ᴄʜᴀʜɪʏᴇ,
+ᴀɢᴀʀ ʙᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴀ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ɴʜɪ ᴋᴀʀ ʀᴀʜᴇ ʜᴀɪɴ ᴛᴏ ᴜɴʜᴇ ᴘᴇʀꜱᴏɴᴀʟ ᴍꜱɢ ᴋᴀʀᴇɴ.
+👉 @Abdul_khalik_7''')
---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/lazyindu/Movie-kh-bot /LazyDeveloperr
+cd /LazyDeveloperr
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
